59acefe970c13f6b7f7bc316bdeead0493abdc95
AvSidwell <sidwellavuyilebatyi@gmail.com>
2023-05-16 09:56:45 +0200

    second_commit

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-let number = 50{
-let square = number * number 
-console.log()
-}
-
 function print () {
-    let num = 60 
+    let num = 20 
     let  num2 = num * num
 console.log(num2)
-}
+};
 
 print()
 
+
+const interestRate = 0.3;
+console.log (interestRate)
+
+const person = { myName: 'Nadine',
+                 age: 19,
+               id:0112125929187
+            };
+
+let { myName, age, id} = person;
+console.log(myName);
+console.log(age);
+console.log(id);
